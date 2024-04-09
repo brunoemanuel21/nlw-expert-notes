9695225c4490048e3d3cb2e1e53c31d32916da9c
brunoemanuel21 <lima2575@outlook.com>
2024-04-09 15:53:32 -0300

    primeiro commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import logo from './assets/Logo.svg'
+
+export function App() {
+  return (
+    <img src={logo}/>
+    <input type="text" placeholder='Busque em suas notas...'/>
+
+    
+  )
+  
+}
+
+
