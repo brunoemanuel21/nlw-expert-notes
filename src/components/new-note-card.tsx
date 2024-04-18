f23bffd27c03dd38f3277da956c4c86f0923ae63
brunoemanuel21 <lima2575@outlook.com>
2024-04-18 16:44:15 -0300

    fim do dia

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -9,6 +9,7 @@ interface NewNoteCardProps {
 
 export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
   const [ShouldShowOnboarding, setShouldShowOnboarding] = useState(true) 
+  const [isRecording, setIsRecording] = useState(false)
   const [content, setContent] = useState('')
 
   function handleStartEditor() {
@@ -33,6 +34,10 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
     setShouldShowOnboarding(true)
 
     toast.success('Nota criada com sucesso!')
+  }
+
+  function hancleStartRecording(){
+    setIsRecording(true)
   }
   
   return(
@@ -61,7 +66,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
 
             {ShouldShowOnboarding ? (
               <p className="text-sm leading-6 text-slate-400">
-              Comece <button className="font-medium text-lime-400">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400">utilize apenas texto</button>.
+              Comece <button onClick={} className="font-medium text-lime-400">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400">utilize apenas texto</button>.
             </p>
             ) : (
               <textarea
@@ -74,12 +79,22 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
             )}
           </div>
 
+          {isRecording ? (
+
           <button 
             type="submit"
             className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
           >
             Salvar nota
           </button>
+          ) : (
+            <button 
+            type="submit"
+            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+          >
+            Salvar nota
+          </button>
+          ) }
           </form>
 
         </Dialog.Content>
